8cafeaf8f1b37ce9508168f6f074cdbb20c7884f
Tejaslakhani52 <tejaslakhani52@gmail.com>
2024-01-24 14:49:47 +0530

    dvcdsvds

--- next.config.js
+++ next.config.js
@@ -1,15 +1,26 @@
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
+// const withOptimizedImages = require("next-optimized-images");
 
-module.exports = withBundleAnalyzer({
+// const withBundleAnalyzer = require("@next/bundle-analyzer")({
+//   enabled: process.env.ANALYZE === "true",
+// });
+
+// module.exports = withBundleAnalyzer({
+//   env: {
+//     NEXT_PUBLIC_ENV: "PRODUCTION",
+//   },
+//   compilerOptions: {
+//     baseUrl: "src",
+//   },
+//   images: {
+//     domains: ["panel.craftyartapp.com", "beta.craftyartapp.com"],
+//   },
+// });
+
+module.exports = {
   env: {
     NEXT_PUBLIC_ENV: "PRODUCTION",
   },
   compilerOptions: {
     baseUrl: "src",
   },
-  images: {
-    domains: ["panel.craftyartapp.com", "beta.craftyartapp.com"],
-  },
-});
+};
